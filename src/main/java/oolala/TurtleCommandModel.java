525cb59aa5c3e07593de385186da7c03a8152092
Zhiyun (David) Lu <zl303@duke.edu>
2022-09-25 19:21:39 -0400

    Some Basic Methods added (Modified from example_browser)

---
+++ src/main/java/oolala/TurtleCommandModel.java
@@ -0,0 +1,6 @@
+package oolala;
+
+
+public class TurtleCommandHistoryModel {
+
+}
---
+++ src/main/java/oolala/TurtleCommandView.java
@@ -0,0 +1,101 @@
+package oolala;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+
+/**
+ * Class for handling command line input
+ * Primary target feature for now: Line by line input and running
+ * Inspired by example_browser project
+ * by Robert C. Duvall
+ * Owen Astrachan
+ * Marcin Dobosz
+ * Yuzhang Han
+ * Edwin Ward
+ *
+ * @author David Lu
+ */
+public class TurtleCommandView {
+
+  public static final int FIELD_WIDTH = 40;
+  public static final String BUTTON_NAME_RUN = "Run";
+  public static final String BUTTON_NAME_CLEAR = "Clear";
+
+  //Useful constants
+
+  //Class variables
+  private TextField mySingleLineInput;
+  private String myErrorMessage;
+  //Subject to change
+  private TurtleCommandModel myModel;
+  //Blank saved for some way to manage the turtles
+  //There should be a list of all turtles and a list of current turtles
+  //Or you can plug in seperated classes for managing turtles in the methods
+
+
+  //methods we might need
+  private void next (){
+
+  }
+  private void back (){
+
+  }
+  private void run (String command){
+    //run command, clear and update error message
+  }
+
+  private void clear (){
+
+  }
+
+
+  //Handles special keys for tracing history or running
+  public void handleKeyInput (KeyCode code) {
+    switch (code) {
+      case UP -> back();
+      case DOWN -> next();
+      case ENTER -> run(mySingleLineInput.getText());
+    }
+  }
+
+
+  // Button creation code simplified from Robert C. Duvall
+  // Use original code for more complex behavior
+  private Button makeButton (String name, EventHandler<ActionEvent> handler){
+    Button result = new Button(name);
+    result.setOnAction(handler);
+    return result;
+  }
+
+  // Typical code to create text field for input from Robert C. Duvall
+  // For single line input
+  private TextField makeInputField (int width, EventHandler<KeyEvent> handler) {
+    TextField result = new TextField();
+    result.setPrefColumnCount(width);
+    result.setOnKeyPressed(handler);
+    return result;
+  }
+
+
+  // Make user-entered text field and buttons
+  // Modified from Robert C. Duvall's code
+  // For single line case
+  // Go take the returned node to add to scene
+  public Node makeInputPanel () {
+    HBox result = new HBox();
+    mySingleLineInput = makeInputField(FIELD_WIDTH, event -> handleKeyInput(event.getCode()));
+    Button runButton = makeButton(BUTTON_NAME_RUN, event -> run(mySingleLineInput.getText()));
+    Button clearButton = makeButton(BUTTON_NAME_CLEAR, event -> clear());
+    result.getChildren().add(clearButton);
+    result.getChildren().add(mySingleLineInput);
+    result.getChildren().add(runButton);
+    return result;
+  }
+
+}
