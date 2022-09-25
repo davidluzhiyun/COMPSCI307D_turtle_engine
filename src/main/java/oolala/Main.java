214d70150318f20bdf6a8ce1a19efe6dde524240
Nishant Wangneo <njw30@duke.edu>
2022-09-25 13:44:18 -0400

    Basic SplashScreen functionality( derived from Breakout Game)

--- src/main/java/oolala/Main.java
+++ src/main/java/oolala/Main.java
@@ -2,8 +2,12 @@ package oolala;
 
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -16,6 +20,7 @@ public class Main extends Application {
    Stage mainStage;
    Timeline animation;
    BorderPane borderPane = new BorderPane();
+
    int mainWidth=600;
    int mainHeight=600;
 
@@ -34,13 +39,37 @@ public class Main extends Application {
         //splashScreen = placeSplash(); // needs to return a group and a scene and will take in a stage
 
         mainStage.setScene(getMainScene());
-        mainStage.setTitle("Nish's mega awesome breakout gamefr");
+        mainStage.setTitle("Turtle Time");
         mainStage.show();
         animation = new Timeline();
         animation.setCycleCount(Timeline.INDEFINITE);
         animation.getKeyFrames().add(new KeyFrame(Duration.seconds((double) 1 / 60), e -> repeat()));
         animation.play();
     }
+
+  public Scene getSplashScreen(){
+    Text title = new Text("!");
+    title.setFont(new Font(60));
+    title.setTextAlignment(TextAlignment.CENTER);
+    title.setX(85);
+    title.setY(200);
+    title.setFill(Color.RED);
+    Button playApp = new Button("play App");
+    playApp.setFont(new Font(20));
+    playApp.setTextAlignment(TextAlignment.CENTER);
+    playApp.setLayoutX(200);
+    playApp.setLayoutY(400);
+    playApp.setOnMouseClicked(e -> startApp());
+
+    Group titleScreen = new Group(title,playApp);
+    return new Scene(titleScreen, mainWidth, mainHeight,Color.BLACK);
+    }
+
+
+    public void startApp(){
+
+    }
+
 
     public Scene getMainScene(){
         Text test = new Text("Hello World");
