469e4221b2c2c26c5529662ff4c4cff8a84dc739
Nishant Wangneo <njw30@duke.edu>
2022-09-25 13:02:55 -0400

    Basic Text and background

--- src/main/java/oolala/Main.java
+++ src/main/java/oolala/Main.java
@@ -2,23 +2,25 @@ package oolala;
 
 
 import javafx.application.Application;
-import javafx.scene.control.Button;
-import javafx.scene.text.Font;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Shape;
 import javafx.util.Duration;
 import java.io.IOException;
-import java.util.ArrayList;
+
 public class Main extends Application {
+   Stage mainStage;
+   Timeline animation;
+   BorderPane borderPane = new BorderPane();
+   int mainWidth=600;
+   int mainHeight=600;
+
+
+
     /**
      * A method to test (and a joke :).
      */
@@ -28,6 +30,26 @@ public class Main extends Application {
 
     @Override
     public void start (Stage stage) throws IOException{
+        mainStage = stage;
+        //splashScreen = placeSplash(); // needs to return a group and a scene and will take in a stage
+
+        mainStage.setScene(getMainScene());
+        mainStage.setTitle("Nish's mega awesome breakout gamefr");
+        mainStage.show();
+        animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(new KeyFrame(Duration.seconds((double) 1 / 60), e -> repeat()));
+        animation.play();
+    }
+
+    public Scene getMainScene(){
+        Text test = new Text("Hello World");
+        borderPane.setCenter(test);
+        Scene scene = new Scene(borderPane,mainWidth,mainHeight,Color.LIGHTBLUE);
+        return scene;
+    }
+
+    private void repeat(){
 
     }
     public static void main (String[] args) {
