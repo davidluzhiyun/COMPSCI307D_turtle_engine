46a85ed460ea4391694935ca22beada42071c8a4
Nishant Wangneo <njw30@duke.edu>
2022-09-25 11:25:14 -0400

    Imports and basic set up

--- src/main/java/oolala/Main.java
+++ src/main/java/oolala/Main.java
@@ -1,10 +1,24 @@
 package oolala;
 
 
-/**
- * Feel free to completely change this code or delete it entirely. 
- */
-public class Main {
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Shape;
+import javafx.util.Duration;
+import java.io.IOException;
+import java.util.ArrayList;
+public class Main extends Application {
     /**
      * A method to test (and a joke :).
      */
@@ -12,10 +26,11 @@ public class Main {
         return 0.001;
     }
 
-    /**
-     * Start of the program.
-     */
+    @Override
+    public void start (Stage stage) throws IOException{
+
+    }
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        launch();
     }
 }
